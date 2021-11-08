73b8915b769e0c5cac083e8f99e53b661c5a37ac
Konstantin Lemko <lemko99@bk.ru>
2021-11-08 16:54:52 +0300

    refactor: use destructuring assignment in 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,16 @@
 
-const path = require('path')
-const fs = require('fs/promises');
+const {join} = require('path')
+const {mkdir, readdir, copyFile} = require('fs/promises');
 
 async function copyDir() {
   try {
-    fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
-    const files = await fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true});
+    await mkdir(join(__dirname, 'files-copy'), {recursive: true});
+    const files = await readdir(join(__dirname, 'files'), {withFileTypes: true});
     for(const file of files) {
       if(file.isFile()) {
-        const filePath = path.join(__dirname, 'files', file.name);
-        const copyPath = path.join(__dirname, 'files-copy', file.name);
-        fs.copyFile(filePath, copyPath);
+        const filePath = join(__dirname, 'files', file.name);
+        const copyPath = join(__dirname, 'files-copy', file.name);
+        copyFile(filePath, copyPath);
       }
     }
   } catch(err) {
