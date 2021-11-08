1dd94a2899a93c24232f95a51991f03ba0889f44
Konstantin Lemko <lemko99@bk.ru>
2021-11-08 16:49:03 +0300

    refactor: change callbacks on promises in 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,17 @@
-const path = require('path')
-const fs = require('fs');
+const {join, parse} = require('path')
+const {readdir, stat} = require('fs/promises');
 
 async function readFolder() {
   try {
-    const files = await fs.promises.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
+    const files = await readdir(join(__dirname, 'secret-folder'), {withFileTypes: true});
     for(const file of files) {
       if(file.isFile()) {
-        const filePath = path.join(__dirname, 'secret-folder', file.name);
-        const info = path.parse(filePath);
+        const filePath = join(__dirname, 'secret-folder', file.name);
+        const info = parse(filePath);
         const name = info.name;
         const ext = info.ext.slice(1);
-        fs.stat(filePath, (err, stats) => {
-          console.log(`${name} - ${ext} - ${stats.size} byte `)
-        });
+        const size = await stat(filePath).then((stat) => stat.size);
+        console.log(`${name} - ${ext} - ${size} byte `)
       }
     }
   } catch(err) {
